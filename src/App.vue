473e4c317e57114b2e4f35c78243997b8b65c087
Fadeta <82428061+Fadeta@users.noreply.github.com>
2023-02-05 16:43:15 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/itts-big.png" alt="Logo" width="55" height="70" class="d-inline-block align-text-top">
+        </a>
+    
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+       </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav align-items-lg-center align-items-start ml-auto">
+            <li class="nav-item">
+              <router-link :to="{name: 'home'}" class="nav-link text-white text-bold" aria-current="page">HOME</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'views.index'}" class="nav-link text-white text-bold">DAFTAR PINJAMAN</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- render vue router -->
+    <router-view></router-view>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+ body {
+   background-color: rgb(255, 255, 255) !important;
+ }
+
+ .mt-custom {
+   margin-top: 110px;
+ }
+ 
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="banner">
+    <div class="container mt-custom">
+      <h1 class="font-weight-bold">WEB INVENTARIS IT TELKOM SURABAYA</h1>
+      <h6 class="text-dark font-weight-normal text-muted pb-3"> Web Inventaris IT Telkom Surabaya, membantu Anda memantau dan mengelola barang-barang dengan mudah dan efisien. Aplikasi ini mempermudah Anda untuk melakukan tugas seperti melihat stok barang, melakukan peminjaman barang, dan melakukan pengembalian barang. </h6>
+      <img src="../assets/Group171.svg" alt="" class="img-fluid">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.banner {
+    text-align: center;
+}
+
+h6{
+  padding: 20px;
+  padding-bottom: 20px;
+  margin: 10px;
+}
+
+h1{
+  margin: 5px;
+  color: rgb(0, 0, 0);
+}
+</style>
